1c5597ea68e9d9f57c4dcc59abdb4367d7f07167
chachakawooka <chachakawooka@googlemail.com>
2019-08-22 19:32:16 +0100

    made manifesto into carousel

--- src/components/manifesto.js
+++ src/components/manifesto.js
@@ -2,13 +2,22 @@ import Card from "@liberaldemocrats/card"
 import React from "react"
 import Img from "gatsby-image"
 import styles from './manifesto.module.scss'
+import Carousel from 'nuka-carousel'
 
 class Manifesto extends React.Component {
 
     render(){
         const issues = this.props.issues;
+
+        let slides = 3;
+        if(window){
+            slides = Math.floor(window.innerWidth / 300) 
+        }
+        
         return(
             <div className={styles.manifesto}>
+                <h2>National Policies</h2>
+                <Carousel  slidesToShow={slides}  cellSpacing={20} >
             {issues.map(({ node }) => (
               <Card
                 img={
@@ -20,6 +29,7 @@ class Manifesto extends React.Component {
                 hovertext={node.subTitle}
                 linktext={node.title} />
             ))}
+            </Carousel>
           </div>
         )
     }
